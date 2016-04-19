2558192e412ddeeb7734c8906dc2b3596c1824fc
salisbum <megan.a.salisbury@gmail.com>
2016-04-19 11:08:08 -0400

    Testing framework starting point

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,30 @@
+require 'coveralls'
+require 'database_cleaner'
+
+Coveralls.wear!('rails')
+
+require File.expand_path("../../config/environment", __FILE__)
+
 RSpec.configure do |config|
+  require File.expand_path("../../config/environment", __FILE__)
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
-  
+
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
 
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+
   config.before(:suite) do
-  DatabaseCleaner.strategy = :transaction
-  DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
-  end
+  config.before(:each) { DatabaseCleaner.start }
+
+  config.after(:each) { DatabaseCleaner.clean }
 end
