ee37f46f70b3cf5d24686b3bb9120690d9313564
salisbum <megan.a.salisbury@gmail.com>
2016-04-22 12:52:09 -0400

    styling updates

--- spec/features/Comments/user_views_comments_on_recipe_spec.rb
+++ spec/features/Comments/user_views_comments_on_recipe_spec.rb
@@ -2,23 +2,23 @@ require "rails_helper"
 
 =begin
 As a user
-I want to view the comments for a recipe
+I want to view the notes for a recipe
 So that I can see what I thought of the recipe
 
 Acceptance Criteria:
 - I must be on the recipe detail page
-- I must only see comments to the recipe I'm viewing
-- I must only see comments to the recipe I'm viewing
-- I must see all comments listed in order, most recent first
+- I must only see notes to the recipe I'm viewing
+- I must only see notes to the recipe I'm viewing
+- I must see all notes listed in order, most recent first
 =end
 
-feature "User views all comments on a recipe" do
-  scenario "User views all comments on a recipe" do
+feature "User views all notes on a recipe" do
+  scenario "User views all notes on a recipe" do
     user = FactoryGirl.create(:user)
     FactoryGirl.create(:profile, user: user)
     recipe = FactoryGirl.create(:recipe, user: user)
-    comment = FactoryGirl.create(:comment, user: user, recipe: recipe)
-    comment2 = FactoryGirl.create(:comment, user: user, recipe: recipe)
+    note = FactoryGirl.create(:comment, user: user, recipe: recipe)
+    note2 = FactoryGirl.create(:comment, user: user, recipe: recipe)
 
     login(user)
 
@@ -26,6 +26,6 @@ feature "User views all comments on a recipe" do
 
     click_link recipe.name
 
-    expect(comment2.body).to appear_before comment.body
+    expect(note2.body).to appear_before note.body
   end
 end
