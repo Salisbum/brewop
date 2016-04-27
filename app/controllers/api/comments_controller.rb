a675b1ceec0faa538ec1ec7d188bc1ddfa6b54c2
salisbum <megan.a.salisbury@gmail.com>
2016-04-27 14:51:49 -0400

    AJAX

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -10,22 +10,6 @@ class Api::CommentsController < ApplicationController
     render json: { comments: @comments }
   end
 
-  # def create
-  #   @comment = Comment.new(comment_params)
-  #   @comment.recipe = recipe
-  #   @comment.user = current_user
-  #
-  #   if @comment.save
-  #     render json: { comments: @comments,
-  #       status: :created,
-  #       location: api_recipe_comment_url(@recipe, @comment)
-  #     }
-  #   else
-  #     render json: @comment.errors,
-  #       status: :unprocessable_entity
-  #   end
-  # end
-
   def edit
     authorize_user
     @comment = Comment.find(params[:id])
@@ -34,6 +18,7 @@ class Api::CommentsController < ApplicationController
   def update
     authorize_user
     @comment = Comment.find(params[:id])
+
     if current_user == @comment.user || current_user.admin?
       if @comment.update(comment_params)
         render json: nil, status: :ok
